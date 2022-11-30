d12b9ecb0ee1fc4e8124d57a4ce02572432bcbe4
rami albasha <rami.albasha@inrae.fr>
2022-11-30 15:52:38 +0100

    mv build_mtg to main_preprocess.py

--- example/potted_grapevine/main_preprocess.py
+++ example/potted_grapevine/main_preprocess.py
@@ -5,14 +5,26 @@ simple shoot architecture.
 from json import dump
 from pathlib import Path
 
-from hydroshoot import io, initialisation
-from openalea.mtg.mtg import MTG
+from hydroshoot import io, initialisation, architecture, display
+from openalea.mtg import traversal, mtg
 from openalea.plantgl.all import Scene
 
-from example.potted_grapevine.sim import build_mtg
+
+def build_mtg(path_file: Path, is_show_scene: bool = True) -> (mtg.MTG, Scene):
+    grapevine_mtg = architecture.vine_mtg(file_path=path_file)
+
+    for v in traversal.iter_mtg2(grapevine_mtg, grapevine_mtg.root):
+        architecture.vine_phyto_modular(grapevine_mtg, v)
+        architecture.vine_mtg_properties(grapevine_mtg, v)
+        architecture.vine_mtg_geometry(grapevine_mtg, v)
+        architecture.vine_transform(grapevine_mtg, v)
+
+    # Display of the plant mock-up (result in 'fig_01_plant_mock_up.png')
+    mtg_scene = display.visu(grapevine_mtg, def_elmnt_color_dict=True, scene=Scene(), view_result=is_show_scene)
+    return grapevine_mtg, mtg_scene
 
 
-def preprocess_inputs(grapevine_mtg: MTG, path_project_dir: Path, psi_soil: float, gdd_since_budbreak: float,
+def preprocess_inputs(grapevine_mtg: mtg.MTG, path_project_dir: Path, psi_soil: float, gdd_since_budbreak: float,
                       scene: Scene):
     inputs = io.HydroShootInputs(g=grapevine_mtg, path_project=path_project_dir, scene=scene, psi_soil=psi_soil,
                                  gdd_since_budbreak=gdd_since_budbreak)
--- example/potted_grapevine/sim.py
+++ example/potted_grapevine/sim.py
@@ -2,29 +2,26 @@
 simple shoot architecture.
 """
 
+from json import load
 from pathlib import Path
 
-from openalea.mtg import traversal, mtg
-from openalea.plantgl.all import Scene
+from hydroshoot import model
 
-from hydroshoot import architecture, display, model
-
-
-def build_mtg(path_file: Path, is_show_scene: bool = True) -> (mtg.MTG, Scene):
-    grapevine_mtg = architecture.vine_mtg(file_path=path_file)
-
-    for v in traversal.iter_mtg2(grapevine_mtg, grapevine_mtg.root):
-        architecture.vine_phyto_modular(grapevine_mtg, v)
-        architecture.vine_mtg_properties(grapevine_mtg, v)
-        architecture.vine_mtg_geometry(grapevine_mtg, v)
-        architecture.vine_transform(grapevine_mtg, v)
-
-    # Display of the plant mock-up (result in 'fig_01_plant_mock_up.png')
-    mtg_scene = display.visu(grapevine_mtg, def_elmnt_color_dict=True, scene=Scene(), view_result=is_show_scene)
-    return grapevine_mtg, mtg_scene
-
+from example.potted_grapevine.main_preprocess import build_mtg
 
 if __name__ == '__main__':
     path_project = Path(__file__).parent
-    g, scene = build_mtg(path_file=path_project / 'digit.csv')
-    summary_results = model.run(g=g, wd=path_project, scene=scene, psi_soil=-0.5, gdd_since_budbreak=1000.)
+    path_preprocessed_data = path_project / 'preprocessed_inputs'
+
+    g, scene = build_mtg(path_file=path_project / 'grapevine_pot.csv', is_show_scene=False)
+
+    with open(path_preprocessed_data / 'static.json') as f:
+        static_inputs = load(f)
+    with open(path_preprocessed_data / 'dynamic.json') as f:
+        dynamic_inputs = load(f)
+
+    summary_results = model.run(g=g, wd=path_project, scene=scene, psi_soil=-0.5, gdd_since_budbreak=1000.,
+                                form_factors=static_inputs['form_factors'],
+                                leaf_nitrogen=static_inputs['Na'],
+                                leaf_ppfd=dynamic_inputs,
+                                path_output=path_project / 'output' / 'time_series_with_preprocessed_data.csv')
