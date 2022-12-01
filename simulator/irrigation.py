fd7061e5dd151785924c27a842791f92acf531d2
rami albasha <rami.albasha@inrae.fr>
2022-12-01 17:04:32 +0100

    add docstring

--- simulator/irrigation.py
+++ simulator/irrigation.py
@@ -3,6 +3,22 @@ from datetime import datetime
 
 def handle_irrigation(date_sim: datetime, date_start_irrigation: datetime, irrigation_freq: int, sapflow: list,
                       drip_rate: float, replacement_fraction: float, irrigation_to_apply: float = 0.) -> (float, float):
+    """
+    Calculates the applied and remaining irrigation rates on a given day, depending on irrigation requirements and
+    drip rate.
+
+    Args:
+        date_sim: current simulated datetime
+        date_start_irrigation: date on which irrigation starts
+        irrigation_freq: number of days between two consecutive irrigation applications.
+        sapflow: [kg s-1] sap flow timeseries
+        drip_rate: [kg h-1] nominal rate of the dripper (one drip per vine is assumed)
+        replacement_fraction: [-] fraction of plant water requirements fulfillment (0 for no irrigation, 1 for complete fulfillment)
+        irrigation_to_apply: [kg h-1] irrigation remaining from the previous time step
+
+    Returns:
+
+    """
     days_since_irrigation_start = (date_sim - date_start_irrigation).days
     if all((days_since_irrigation_start >= 0,
             days_since_irrigation_start % irrigation_freq == 0,
