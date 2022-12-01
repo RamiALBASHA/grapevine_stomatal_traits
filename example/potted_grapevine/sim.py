dc660a90a3b3fc2d8426b55053b4ae8ec3435e22
rami albasha <rami.albasha@inrae.fr>
2022-12-01 17:05:34 +0100

    set simulation example

--- example/potted_grapevine/sim.py
+++ example/potted_grapevine/sim.py
@@ -5,23 +5,27 @@ simple shoot architecture.
 from json import load
 from pathlib import Path
 
-from hydroshoot import model
-
 from example.potted_grapevine.main_preprocess import build_mtg
+from simulator import hydroshoot_wrapper
 
 if __name__ == '__main__':
     path_project = Path(__file__).parent
     path_preprocessed_data = path_project / 'preprocessed_inputs'
 
-    g, scene = build_mtg(path_file=path_project / 'grapevine_pot.csv', is_show_scene=False)
+    g, scene = build_mtg(path_file=path_project / 'digit.csv', is_show_scene=False)
 
     with open(path_preprocessed_data / 'static.json') as f:
         static_inputs = load(f)
     with open(path_preprocessed_data / 'dynamic.json') as f:
         dynamic_inputs = load(f)
 
-    summary_results = model.run(g=g, wd=path_project, scene=scene, psi_soil=-0.5, gdd_since_budbreak=1000.,
-                                form_factors=static_inputs['form_factors'],
-                                leaf_nitrogen=static_inputs['Na'],
-                                leaf_ppfd=dynamic_inputs,
-                                path_output=path_project / 'output' / 'time_series_with_preprocessed_data.csv')
+    summary_results = hydroshoot_wrapper.run(
+        g=g, wd=path_project, scene=scene, gdd_since_budbreak=1000.,
+        form_factors=static_inputs['form_factors'],
+        leaf_nitrogen=static_inputs['Na'],
+        leaf_ppfd=dynamic_inputs,
+        # psi_soil_init=-0.5,
+        drip_rate=3.8,
+        replacement_fraction=0.6,
+        irrigation_freq=2,
+        path_output=path_project / 'output' / 'time_series_with_preprocessed_data.csv')
