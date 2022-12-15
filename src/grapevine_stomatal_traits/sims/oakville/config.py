4379d852f5c89769dc7ec9a7d11bb52f407b7d1b
rami albasha <rami.albasha@inrae.fr>
2022-12-15 15:39:54 +0100

    add config to oakville sims

---
+++ src/grapevine_stomatal_traits/sims/oakville/config.py
@@ -0,0 +1,17 @@
+from grapevine_stomatal_traits.sources.config import SiteData
+
+
+class SiteDataOakville(SiteData):
+    def __init__(self):
+        super(SiteDataOakville, self).__init__(
+            date_start_sim='1990-07-30 20:00:00',
+            date_end_sim='1990-08-30 23:00:00',
+            date_budburst='1990-04-01 00:00:00',
+            latitude=38.43,
+            longitude=-122.41,
+            elevation=58,
+            soil_class='Clay_Loam',
+            spacing_interrow=1.8,
+            spacing_intrarow=1.8,
+            soil_depth=2.,
+            rhyzo_coeff=0.75)
