e4c104834ff41aaab4e40183301ef7f736e87fb6
rami albasha <rami.albasha@inrae.fr>
2022-12-15 17:06:19 +0100

    set dates for fresno

--- src/grapevine_stomatal_traits/sims/fresno/config.py
+++ src/grapevine_stomatal_traits/sims/fresno/config.py
@@ -4,8 +4,8 @@ from grapevine_stomatal_traits.sources.config import SiteData
 class SiteDataFresno(SiteData):
     def __init__(self):
         super(SiteDataFresno, self).__init__(
-            date_start_sim='1990-07-12 12:00:00',
-            date_end_sim='1990-07-12 12:00:00',
+            date_start_sim='1990-07-12 00:00:00',
+            date_end_sim='1990-08-11 23:00:00',
             date_budburst='1990-04-01 00:00:00',
             latitude=36.82,
             longitude=-119.74,
