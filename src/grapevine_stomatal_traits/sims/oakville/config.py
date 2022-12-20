152f00d986dced3fac9cefa6e3c4491a3ffbbe96
rami albasha <rami.albasha@inrae.fr>
2022-12-20 17:16:02 +0100

    modify the starting date for oakville

--- src/grapevine_stomatal_traits/sims/oakville/config.py
+++ src/grapevine_stomatal_traits/sims/oakville/config.py
@@ -4,7 +4,7 @@ from grapevine_stomatal_traits.sources.config import SiteData
 class SiteDataOakville(SiteData):
     def __init__(self):
         super(SiteDataOakville, self).__init__(
-            date_start_sim='1990-07-30 20:00:00',
+            date_start_sim='1990-07-31 00:00:00',
             date_end_sim='1990-08-30 23:00:00',
             date_budburst='1990-04-01 00:00:00',
             latitude=38.43,
