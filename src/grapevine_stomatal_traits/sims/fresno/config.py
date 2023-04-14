969781bb65066f78ad9159cbecc8ac28dba2acd5
rami albasha <rami.albasha@inrae.fr>
2023-04-14 12:00:22 +0200

    set 'root_length' of fresno to 300 m

--- src/grapevine_stomatal_traits/sims/fresno/config.py
+++ src/grapevine_stomatal_traits/sims/fresno/config.py
@@ -18,7 +18,7 @@ class SiteDataFresno(SiteData):
             soil_depth=2.,
             rhyzo_coeff=0.65,
             avg_root_radius=0.001,
-            root_length=200,
+            root_length=300,
             initial_soil_water_potential=-0.6)
 
 
